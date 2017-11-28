9a25af0ea7a4bf231fe98a6763d7bad336223692
Matt Cheung <matt.cheung94@hotmail.co.uk>
2017-11-28 12:04:21 +0000

    Dwarfs and Dwarves

---
+++ 03-01-JavaDwarfsAndDwarves/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] arg) {
+		ArrayList<String> dwarfs = new ArrayList<String>(7);
+		ArrayList<String> dwarves = new ArrayList<String>(13);
+
+		dwarfs.add("Doc");
+		dwarfs.add("Grumpy");
+		dwarfs.add("Happy");
+		dwarfs.add("Sleepy");
+		dwarfs.add("Dopey");
+		dwarfs.add("Bashful");
+		dwarfs.add("Sneezy");
+
+		dwarves.add("Dwalin");
+		dwarves.add("Balin");
+		dwarves.add("Kili");
+		dwarves.add("Fili");
+		dwarves.add("Doro");
+		dwarves.add("Nori");
+		dwarves.add("Ori");
+		dwarves.add("Oin");
+		dwarves.add("Gloin");
+		dwarves.add("Bifur");
+		dwarves.add("Bofur");
+		dwarves.add("Bombur");
+		dwarves.add("Thorin");
+
+		System.out.println("THe dwarfs in Snow White are: ");
+		for (int i = 0; i < dwarfs.size(); i++)
+			System.out.println(dwarfs.get(i));
+		
+		System.out.println("");
+		System.out.println("THe dwarves in THorin Oakenshield's party are: ");
+		for (int i = 0; i < dwarves.size(); i++)
+			System.out.println(dwarves.get(i));
+	}
+}
