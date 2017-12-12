74a4827073c39a3f0aeb80ec7998c4cb512a96f1
Matt Cheung <matt.cheung94@hotmail.co.uk>
2017-12-12 09:21:05 +0000

    made cereal constructor, still setting up html

--- 06-01-Form/src/main/java/uk/ac/belfastmet/form/controller/HomeController.java
+++ 06-01-Form/src/main/java/uk/ac/belfastmet/form/controller/HomeController.java
@@ -3,6 +3,7 @@ package uk.ac.belfastmet.form.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,11 +17,20 @@ public class HomeController {
 		return "formPage";
 	}
 	
-	@GetMapping("/results")
+	@PostMapping("/results")
 	public String result(@RequestParam("firstname") String firstName, @RequestParam("lastname") String lastName, @RequestParam("gender") String gender, Model model) {
 		model.addAttribute("firstName", firstName);
 		model.addAttribute("lastName", lastName);
 		model.addAttribute("gender", gender);
+		return "resultPage";
+	}
+	
+	@GetMapping("/results")
+	public String getresults(Model model) {
+		
+		model.addAttribute("firstName", "Chuck");
+		model.addAttribute("lastName", "Norris");
+		model.addAttribute("gender", "All of them");
 		return "resultPage";
 	}
 }
